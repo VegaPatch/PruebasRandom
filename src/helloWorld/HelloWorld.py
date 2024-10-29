6d9c9c7fc9282a0ce476d1dbe00f8ab763ba67b6
Adrian Fernandez <adrian.fernandezm@alten.es>
2024-10-29 12:40:56 +0100

    added introduction and farewell

--- src/helloWorld/HelloWorld.py
+++ src/helloWorld/HelloWorld.py
@@ -1,7 +1,9 @@
 from greetings import greet
 
+print("Welcome user, please let me know your name to continue")
+
 def main():
-    user_name = input("¿What is your name? ")
+    user_name = input("Introduce your name: ")
     greet(user_name)
 
 if __name__ == "__main__":
