6e227726790180c012eca14526ebdc0b9e28b4f7
Alejandro Castillo <alejandro.castillo@alten.es>
2024-10-29 10:43:42 +0100

    added handleExceptions
    Now when you introduce wrong usernames the program return a error message and tell the user where is his error

--- src/helloWorld/HelloWorld.py
+++ src/helloWorld/HelloWorld.py
@@ -1,8 +1,27 @@
-from greetings import greet
+import re
+from greetings import greet  
 
 def main():
-    user_name = input("¿What is your name? ")
-    greet(user_name)
+    while True:
+        user_name = input("¿What is your name? ")       
+        if len(user_name) < 3:
+            print("The username has to have more than 3 characters")
+            continue
+        if not user_name[0].isupper():
+            print("You cannot enter your name with the first letter in lowercase!")
+            continue
+        if validate(user_name):
+            greet(user_name)
+            break
+        else:
+            print("Invalid characters in your username")
+
+def validate(name):
+    valid_characters = r'^[a-zA-Z]+$'
+    if re.match(valid_characters, name):
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
